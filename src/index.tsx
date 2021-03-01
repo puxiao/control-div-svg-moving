917fe483ec9f66eae079d7d31c84a436323cb232
puxiao <yangpuxiao@gmail.com>
2021-03-01 04:04:47 -0600

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './index.scss'
+
+const points = "M20,20 C20,100 200,0 200,100" //所有的路径关键点
+const length = 230 //路径总长度
+
+const SVGAnimation = () => {
+    const [num, setNum] = useState<number>(1)
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNum(Number(event.target.value))
+    }
+    return (
+        <div>
+            <div className='over-icon' style={{ offsetDistance: `${num}%` }}></div>
+            <svg className='both-svg over-svg' viewBox="0 0 280 150">
+                <path d={points} style={{ strokeDasharray: length, strokeDashoffset: `${100 - num}%` }} />
+            </svg>
+            <svg className='both-svg' viewBox="0 0 280 150">
+                <path d={points} />
+            </svg>
+            <input type='number' value={num} min="1" max="100" step='1' onChange={handleInputChange} />
+        </div>
+    )
+}
+
+export default SVGAnimation
